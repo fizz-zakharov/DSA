e2c888072af93116f182eb4085fa153ef3d9336d
Ayush Sheel <sheelayush15@gmail.com>
2026-01-07 22:07:59 +0530

    Time: 13 ms (7.02%) | Memory: 148.6 MB (5.25%) - LeetSync

--- 1116-maximum-level-sum-of-a-binary-tree/maximum-level-sum-of-a-binary-tree.cpp
+++ 1116-maximum-level-sum-of-a-binary-tree/maximum-level-sum-of-a-binary-tree.cpp
@@ -12,38 +12,29 @@
 class Solution {
 public:
     int maxLevelSum(TreeNode* root) {
-
-        if (root == NULL) return 0;  
-
+        
         queue<pair<TreeNode*,int>> q;
-        vector<long long> v(1e5, 0); 
-
-        q.push({root, 1});
-        int maxLevel = 1;
-
+        vector<long long> v(1e5,0);
+        q.push({root,1});
+        int maxlevel=1;
         while(!q.empty()){
-            TreeNode* node = q.front().first;
-            int level = q.front().second;
+            TreeNode* node=q.front().first;
+            int level= q.front().second;
             q.pop();
-
-            v[level] += node->val;   
-            maxLevel = max(maxLevel, level);
-
-            if(node->left != NULL){
-                q.push({node->left, level + 1});
+            maxlevel = max(maxlevel, level);
+            v[level]+=node->val;
+            if(node->left!=NULL){
+                q.push({node->left,level+1});
             }
-            if(node->right != NULL){
-                q.push({node->right, level + 1});
+            if(node->right!=NULL){
+                q.push({node->right,level+1});
             }
         }
-
-        int ans = 1;
-        long long cur = LLONG_MIN;
-
-        for(int i = 1; i <= maxLevel; i++){   
-            if(v[i] > cur){
-                cur = v[i];
-                ans = i;
+        int ans=0,cur=INT_MIN;
+        for(int i=1;i<=maxlevel;i++){
+            if(v[i]>cur){
+                cur=v[i];
+                ans=i;
             }
         }
         return ans;

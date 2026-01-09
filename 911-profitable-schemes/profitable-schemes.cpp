98dd309beace0ab5db0f6da7cc2c2eee6d1a4074
Ayush Sheel <sheelayush15@gmail.com>
2026-01-09 21:48:50 +0530

    Time: 180 ms (21.46%) | Memory: 56.1 MB (42.26%) - LeetSync

---
+++ 911-profitable-schemes/profitable-schemes.cpp
@@ -0,0 +1,23 @@
+class Solution {
+long long MOD=1e9+7;
+private:
+    int fn(int i,int mem,int sum,int minProfit,vector<int>& group,vector<int>& profit,vector<vector<vector<int>>>&dp){
+        if(i<0)return (sum>=minProfit);
+        if(mem<0)return 0;
+        if(dp[i][mem][sum]!=-1)return dp[i][mem][sum];
+
+        int take = 0;
+        if (mem >= group[i]){
+            take=fn(i-1,mem-group[i],min(sum+profit[i],minProfit),minProfit,group,profit,dp);
+        }
+        int skip = fn(i-1,mem,sum,minProfit,group,profit,dp);
+
+        return dp[i][mem][sum] = (take + skip) % MOD;
+    }
+public:
+    int profitableSchemes(int n, int minProfit, vector<int>& group, vector<int>& profit) {
+        int m=group.size();
+        vector<vector<vector<int>>> dp(m,vector<vector<int>>(n+1,vector<int>(minProfit+1,-1)));
+        return fn(m-1,n,0,minProfit,group,profit,dp);
+    }
+};

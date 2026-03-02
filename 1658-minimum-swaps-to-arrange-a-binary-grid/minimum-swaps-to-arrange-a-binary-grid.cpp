ddf9df33a190368e5b339c58c025ba30ed0371d4
Ayush Sheel <sheelayush15@gmail.com>
2026-03-02 20:30:30 +0530

    Time: 0 ms (100.00%) | Memory: 29.5 MB (93.90%) - LeetSync

---
+++ 1658-minimum-swaps-to-arrange-a-binary-grid/minimum-swaps-to-arrange-a-binary-grid.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int minSwaps(vector<vector<int>>& grid) {
+        int n=grid.size();
+        int c=0;
+        for(int i=0;i<n-1;i++){
+            bool check=false;
+            for(int j=i+1;j<n;j++){
+                if(grid[i][j]){
+                    check=true;
+                }
+            }
+            if(check){ //row is not in the right place and we'll find if the right row exists?
+                bool exists=false;
+                for(int k=i+1;k<n;k++){
+                    bool valid=true;
+                    for(int j=i+1;j<n;j++){
+                        if(grid[k][j]){
+                            valid=false;
+                        }
+                    }
+                    if(valid){
+                        exists=true;
+                        for(int p=k;p>i;p--){
+                            swap(grid[p],grid[p-1]);
+                            c++;
+                        }
+                        break;
+                    }
+                }
+                if(!exists)return -1;
+            }
+        }
+        return c;
+    }
+};

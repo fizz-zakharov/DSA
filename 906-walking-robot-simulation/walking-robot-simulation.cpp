45ce3cb3f56f26921c26d3afbb0987acffe45d3b
Ayush Sheel <sheelayush15@gmail.com>
2026-04-07 03:52:06 +0530

    Time: 86 ms (8.58%) | Memory: 52.9 MB (10.46%) - LeetSync

--- 906-walking-robot-simulation/walking-robot-simulation.cpp
+++ 906-walking-robot-simulation/walking-robot-simulation.cpp
@@ -1,59 +1,58 @@
 class Solution {
 public:
     int robotSim(vector<int>& commands, vector<vector<int>>& obstacles) {
-        int n = commands.size();
-        map<pair<int,int>, int> m;
-        for(auto it : obstacles){
-            m[{it[0], it[1]}]++;
+        int n=commands.size();
+        map<pair<int,int>,int> m;
+        for(auto it:obstacles){
+            m[{it[0],it[1]}]++;
         }
-
-        int dir = 1; // 1=north, 2=east, 3=south, 4=west
-        pair<int,int> coordinates = {0, 0};
-        int ans = 0;  
-
-        for(int i = 0; i < n; i++){
-            int x = coordinates.first;   
-            int y = coordinates.second;
-            int k = commands[i];
-
-            if(commands[i] < 0){
-                if(commands[i] == -2){
+        int dir=1; //dir 1->north,2->east,3->south,4->west cw
+        pair<int,int> coordinates={0,0};
+        int ans=0;  
+        for(int i=0;i<n;i++){
+            int x=coordinates.first;
+            int y=coordinates.second;
+            int k=commands[i];
+            if(commands[i]<0){
+                if(commands[i]==-2){
                     dir--;
-                    if(dir == 0) dir = 4;
-                } else {
+                    if(dir==0)dir=4;
+                }
+                else{
                     dir++;
-                    if(dir == 5) dir = 1;
+                    if(dir==5)dir=1;
                 }
-            } else {
-                if(dir == 1){
-                    for(int step = 1; step <= k; step++){  
-                        if(m[{x, y+1}] > 0) break;       
-                        y++;
+            }
+            else{
+                if(dir==1){
+                    for(int step=1;step<=k;step++){  
+                        if(m[{x,y+1}]>0)break;      
+                        y++;                         
                     }
                 }
-                if(dir == 2){
-                    for(int step = 1; step <= k; step++){  
-                        if(m[{x+1, y}] > 0) break;      
-                        x++;
+                if(dir==2){
+                    for(int step=1;step<=k;step++){  
+                        if(m[{x+1,y}]>0)break;      
+                        x++;                          
                     }
                 }
-                if(dir == 3){
-                    for(int step = 1; step <= k; step++){  
-                        if(m[{x, y-1}] > 0) break;        
-                        y--;
+                if(dir==3){
+                    for(int step=1;step<=k;step++){  
+                        if(m[{x,y-1}]>0)break;     
+                        y--;                          
                     }
                 }
-                if(dir == 4){
-                    for(int step = 1; step <= k; step++){  
-                        if(m[{x-1, y}] > 0) break;        
-                        x--;
+                if(dir==4){
+                    for(int step=1;step<=k;step++){  
+                        if(m[{x-1,y}]>0)break;     
+                        x--;                          
                     }
                 }
-                coordinates.first = x;
-                coordinates.second = y;
-                ans = max(ans, x*x + y*y); 
+                coordinates.first=x;
+                coordinates.second=y;
+                ans=max(ans,x*x+y*y);  
             }
         }
-        return ans;
+        return ans;  
     }
 };

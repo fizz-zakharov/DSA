5505da37f204583020bd42bfe44d3d106fbd42bf
Ayush Sheel <sheelayush15@gmail.com>
2026-05-17 00:09:27 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (45.93%) - LeetSync

--- 140-word-break-ii/word-break-ii.cpp
+++ 140-word-break-ii/word-break-ii.cpp
@@ -1,7 +1,7 @@
 class Solution {
 vector<string> ans;
 private:
-    bool fn(int i, string s, string temp, map<string, int>& m, int n) {
+    bool fn(int i, string s, string temp, map<string, int>& m, int n){
         // base case
         if (i >= n) {
             ans.push_back(temp);
@@ -12,12 +12,12 @@ private:
             string x = s.substr(i,l);
             if (m[x]>0){
                 bool ch = false;
-                if (temp.size() > 0) {
+                if (temp.size() > 0){
                     temp.push_back(' ');
                     ch = true;
                 }
-                temp += x;
-                found |= fn(i + l, s, temp, m, n);
+                temp+=x;
+                found|=fn(i+l,s,temp,m,n);
 
                 // backtrack
                 if(ch)temp.erase(temp.size() - x.size() - 1, x.size() + 1);
@@ -31,7 +31,7 @@ private:
 public:
     vector<string> wordBreak(string s, vector<string>& wordDict) {
         int n = s.size();
-        map<string, int> m;
+        map<string,int> m;
         for (auto it:wordDict)m[it]++;
         string z;
         fn(0,s,z,m,n);

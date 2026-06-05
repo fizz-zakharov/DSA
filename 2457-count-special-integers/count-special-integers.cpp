08aa00f06f769dcc8f17062186b63e21a115dbe1
Ayush Sheel <sheelayush15@gmail.com>
2026-06-05 16:31:32 +0530

    Time: 7 ms (68.29%) | Memory: 17 MB (17.36%) - LeetSync

--- 2457-count-special-integers/count-special-integers.cpp
+++ 2457-count-special-integers/count-special-integers.cpp
@@ -1,9 +1,10 @@
 class Solution {
 private:
-    int fn(int i,int tight,int mask,string& s){
+    int fn(int i,int tight,int mask,string& s,vector<vector<vector<int>>>&dp){
         if(i>=s.size()){
             return 1;
         }
+        if(dp[i][tight][mask]!=-1)return dp[i][tight][mask];
         int ans=0;
         if(tight==1){
             for(int j=0;j<=s[i]-'0';j++){
@@ -17,7 +18,7 @@ private:
                 }
                 int newtight=0;
                 if(j==s[i]-'0')newtight=1;
-                ans+=fn(i+1,newtight,newmask,s);
+                ans+=fn(i+1,newtight,newmask,s,dp);
             }
         }
         else{
@@ -30,14 +31,15 @@ private:
                 else{
                     newmask=mask|(1<<j);
                 }
-                ans+=fn(i+1,0,newmask,s);
+                ans+=fn(i+1,0,newmask,s,dp);
             }
         }
-        return ans;
+        return dp[i][tight][mask]= ans;
     }
 public:
     int countSpecialNumbers(int n) {
         string s=to_string(n);
-        return fn(0,1,0,s)-1;
+        vector<vector<vector<int>>> dp(s.size(),vector<vector<int>>(2,vector<int>(1024,-1)));
+        return fn(0,1,0,s,dp)-1;
     }
 };

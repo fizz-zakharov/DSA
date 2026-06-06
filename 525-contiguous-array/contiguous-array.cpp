38a766990051cdde984dc3bd5e70811359625032
Ayush Sheel <sheelayush15@gmail.com>
2026-06-06 15:09:47 +0530

    Time: 55 ms (5.21%) | Memory: 88.6 MB (15.54%) - LeetSync

--- 525-contiguous-array/contiguous-array.cpp
+++ 525-contiguous-array/contiguous-array.cpp
@@ -2,20 +2,18 @@ class Solution {
 public:
     int findMaxLength(vector<int>& nums) {
         int n=nums.size();
-        vector<int> pre(n);
         map<int,int> m; //{sum,index}
         int sum=0;
         int ans=0;
         for(int i=0;i<n;i++){
             if(nums[i])sum++;
             else sum--;
-            pre[i]=sum;
-            if(pre[i]==0)ans=max(ans,i+1);
-            if(m.find(pre[i])!=m.end()){
-                ans=max(ans,i-m[pre[i]]);
+            if(sum==0)ans=max(ans,i+1);
+            if(m.find(sum)!=m.end()){
+                ans=max(ans,i-m[sum]);
             }
             else{
-                m[pre[i]]=i;
+                m[sum]=i;
             }
         }
         return ans;

1490c382a620535cc680b0fac5a3be1f33fff132
Ayush Sheel <sheelayush15@gmail.com>
2026-06-15 00:16:38 +0530

    Time: 12 ms (28.40%) | Memory: 9.6 MB (43.85%) - LeetSync

---
+++ 1203-print-in-order/print-in-order.cpp
@@ -0,0 +1,37 @@
+class Foo {
+    std::mutex m;
+    std::condition_variable cv;
+    int turn=0;
+public:
+    Foo() {
+        
+    }
+
+    void first(function<void()> printFirst) {
+        
+        // printFirst() outputs "first". Do not change or remove this line.
+        printFirst();
+        turn=1;
+        cv.notify_all();
+    }
+
+    void second(function<void()> printSecond) {
+        std::unique_lock<std::mutex> lock(m);
+        // printSecond() outputs "second". Do not change or remove this line.
+        while(turn!=1){
+            cv.wait(lock);
+        }
+        printSecond();
+        turn=2;
+        cv.notify_all();
+    }
+
+    void third(function<void()> printThird) {
+        std::unique_lock<std::mutex> lock(m);
+        // printThird() outputs "third". Do not change or remove this line.
+        while(turn!=2){
+            cv.wait(lock);
+        }
+        printThird();
+    }
+};

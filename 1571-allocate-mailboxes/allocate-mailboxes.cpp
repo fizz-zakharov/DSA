a92c7fbdb0964b8c0a9c9d2c8a0ba4ff09663ec2
Ayush Sheel <sheelayush15@gmail.com>
2026-06-16 22:06:52 +0530

    Time: 84 ms (5.20%) | Memory: 94 MB (5.20%) - LeetSync

--- 1571-allocate-mailboxes/allocate-mailboxes.cpp
+++ 1571-allocate-mailboxes/allocate-mailboxes.cpp
@@ -10,15 +10,18 @@
 
 class Solution {
 private:
-    long long fn(int i,int j,int k,vector<int>&v,vector<vector<vector<long long>>>&cost,
-    vector<vector<vector<long long>>>&dp){
+    long long fn(int i,int j,int k,vector<int>&v,vector<vector<vector<long long>>>&dp){
         if(i>j)return 0;
         if(k==0)return INT_MAX;
         if(dp[i][j][k]!=-1)return dp[i][j][k];
         long long ans=INT_MAX;
         for(int x=i;x<=j;x++){
             int h=(i+x)/2;
-            ans=min(ans,cost[i][x][h]+fn(x+1,j,k-1,v,cost,dp));
+            int cost=0;
+            for(int p=i;p<=x;p++){
+                cost+=abs(v[p]-v[h]);
+            }
+            ans=min(ans,cost+fn(x+1,j,k-1,v,dp));
         }
         return dp[i][j][k] = ans;
     }
@@ -26,20 +29,9 @@ public:
     int minDistance(vector<int>& houses, int k) {
         int n=houses.size();
         sort(houses.begin(),houses.end());
-        vector<vector<vector<long long>>> cost(n,vector<vector<long long>>(n,vector<long long>(n,0)));
-        //minimum total distance of houses i->j such that mail is on house k
-
-        for(int i=0;i<n;i++){
-            for(int l=i;l<n;l++){
-                cost[i][i][l]=abs(houses[i]-houses[l]); 
-                for(int j=i+1;j<n;j++){
-                    cost[i][j][l]=cost[i][j-1][l]+abs(houses[j]-houses[l]);
-                }
-            }
-        }
 
         vector<vector<vector<long long>>> dp(n,vector<vector<long long>>(n,vector<long long>(n+1,-1)));
     
-        return fn(0,n-1,k,houses,cost,dp);
+        return fn(0,n-1,k,houses,dp);
     }
 };

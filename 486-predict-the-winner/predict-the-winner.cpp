a16ccb78916840adcb5984f8ca95869dab0c0af6
Ayush Sheel <sheelayush15@gmail.com>
2026-06-17 22:29:54 +0530

    Time: 0 ms (100.00%) | Memory: 10.3 MB (28.72%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.cpp
+++ 486-predict-the-winner/predict-the-winner.cpp
@@ -1,18 +1,20 @@
 class Solution {
 private:
-    int fn(int i,int j,vector<int>&v){
+    int fn(int i,int j,vector<int>&v,vector<vector<int>>&dp){
         if(i>j)return 0;
         if(i==j)return v[i];
-        int a=v[i]+min(fn(i+2,j,v),fn(i+1,j-1,v));
-        int b=v[j]+min(fn(i+1,j-1,v),fn(i,j-2,v));
-        return max(a,b);
+        if(dp[i][j]!=-1)return dp[i][j];
+        int a=v[i]+min(fn(i+2,j,v,dp),fn(i+1,j-1,v,dp));
+        int b=v[j]+min(fn(i+1,j-1,v,dp),fn(i,j-2,v,dp));
+        return dp[i][j] = max(a,b);
     }
 public:
     bool predictTheWinner(vector<int>& nums) {
         int n=nums.size();
         int sum=0;
         for(int i=0;i<n;i++)sum+=nums[i];
-        int score=fn(0,n-1,nums);
+        vector<vector<int>> dp(n,vector<int>(n,-1));
+        int score=fn(0,n-1,nums,dp);
         return score>=(sum-score);
     }
 };

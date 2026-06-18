1900d0a765b8b4cb8876969aca179802541c767d
Ayush Sheel <sheelayush15@gmail.com>
2026-06-18 23:51:20 +0530

    Time: 425 ms (15.33%) | Memory: 34.7 MB (12.46%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.cpp
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -1,10 +1,11 @@
 class Solution {
 private:    
-    int fn(int i,int j,string& s){
+    int fn(int i,int j,string& s,vector<vector<int>>&dp){
         if(i>=j){
             if(i==j)return 1;
             return 0;
         }
+        if(dp[i][j]!=-1)return dp[i][j];
         int ans=0;
         for(int k=i;k<=j;k++){
             bool ch=true;
@@ -18,11 +19,12 @@ private:
                 ans++;
             }
         }
-        return ans+fn(i+1,j,s);
+        return dp[i][j] = ans+fn(i+1,j,s,dp);
     }
 public:
     int countSubstrings(string s) {
         int n=s.size();
-        return fn(0,n-1,s);
+        vector<vector<int>> dp(n,vector<int>(n,-1));
+        return fn(0,n-1,s,dp);
     }
 };

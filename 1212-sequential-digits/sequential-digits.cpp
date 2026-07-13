d18266b3878f92088bb92779b2cc56ad8e38b4fd
Ayush Sheel <sheelayush15@gmail.com>
2026-07-13 07:00:34 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (76.91%) - LeetSync

---
+++ 1212-sequential-digits/sequential-digits.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> sequentialDigits(int low, int high) {
+        string digits="123456789";
+        vector<int> ans;
+        for(int i=2;i<=9;i++){
+            for(int j=0;j<=9-i;j++){
+                int num=stoi(digits.substr(j,i));
+                if(num>=low && num<=high)ans.push_back(num);
+            }
+        }
+        return ans;
+    }
+};

f188f751d36dd9f77b3d041bd87ef30bbb794237
Ayush Sheel <sheelayush15@gmail.com>
2026-07-27 19:01:35 +0530

    Time: 53 ms (71.66%) | Memory: 18.4 MB (70.74%) - LeetSync

---
+++ 2044-number-of-wonderful-substrings/number-of-wonderful-substrings.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    long long wonderfulSubstrings(string word) {
+        int n=word.size();
+        long long freq[1024];
+        memset(freq,0,sizeof(freq));
+        int mask=0;
+        long long ans=0;
+        freq[0]=1;
+        for(int i=0;i<n;i++){
+            int k=word[i]-'a';
+            mask=(mask)^(1<<k);
+            ans+=freq[mask];
+            for(int j=0;j<10;j++){
+                ans+=freq[mask^(1<<j)];
+            }
+            freq[mask]++;
+        }
+        return ans;
+    }
+};

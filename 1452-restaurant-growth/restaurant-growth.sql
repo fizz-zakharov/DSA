0a5b7441c1a1428637ee18dc1980514cb1ad680f
Ayush Sheel <sheelayush15@gmail.com>
2026-07-31 16:53:01 +0530

    Time: 355 ms (77.78%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1452-restaurant-growth/restaurant-growth.sql
@@ -0,0 +1,21 @@
+SELECT x.visited_on, (
+    SELECT SUM(amount) FROM (
+        SELECT visited_on, SUM(amount) AS amount
+        FROM Customer
+        GROUP BY visited_on
+    ) AS c
+    WHERE c.visited_on BETWEEN y.visited_on AND x.visited_on
+) AS amount,
+ROUND((
+    SELECT SUM(amount) FROM (
+        SELECT visited_on, SUM(amount) AS amount
+        FROM Customer
+        GROUP BY visited_on
+    ) AS c
+    WHERE c.visited_on BETWEEN y.visited_on AND x.visited_on
+) / 7, 2) AS average_amount
+FROM (SELECT DISTINCT visited_on FROM Customer) AS x
+JOIN (SELECT DISTINCT visited_on FROM Customer) AS y
+  ON DATEDIFF(x.visited_on, y.visited_on) = 6
+GROUP BY x.visited_on, y.visited_on
+ORDER BY x.visited_on;

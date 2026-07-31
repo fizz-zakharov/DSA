f6100cae1faa81be42a56fd9992706c2993f4318
Ayush Sheel <sheelayush15@gmail.com>
2026-07-31 15:50:41 +0530

    Time: 3668 ms (5.00%) | Memory: 0B (100.00%) - LeetSync

---
+++ 626-exchange-seats/exchange-seats.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+SELECT CASE
+    WHEN id%2!=0 AND id=(SELECT max(id) FROM Seat) THEN id
+    WHEN id%2!=0 THEN id+1
+    ELSE id-1
+END AS id,student FROM Seat
+ORDER BY id;

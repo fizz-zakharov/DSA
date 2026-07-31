fb1e18bb07cec802e395380e0e8d70373eb2a542
Ayush Sheel <sheelayush15@gmail.com>
2026-07-31 23:46:44 +0530

    Time: 563 ms (9.84%) | Memory: 0B (100.00%) - LeetSync

--- 1670-patients-with-a-condition/patients-with-a-condition.sql
+++ 1670-patients-with-a-condition/patients-with-a-condition.sql
@@ -1,4 +1,4 @@
 # Write your MySQL query statement below
 SELECT patient_id,patient_name,conditions
 FROM Patients WHERE 
-conditions REGEXP '^DIAB1| DIAB1';
+conditions LIKE 'DIAB1%' or conditions like '% DIAB1%';

21537ac96c00e657f842e8cef4245c246fe0aa4a
Ayush Sheel <sheelayush15@gmail.com>
2026-08-07 16:20:46 +0530

    Time: 7 ms (4.57%) | Memory: 15.4 MB (5.17%) - LeetSync

---
+++ 1081-video-stitching/video-stitching.cpp
@@ -0,0 +1,29 @@
+class Solution {
+private:
+    int dp[102][102][102];
+    int fn(int i,int l,int r,vector<vector<int>>&v,int time){
+        if(r>=time){
+            return 0;
+        }
+        if(i>=v.size())return 1e9;
+        if(dp[i][l][r]!=-1)return dp[i][l][r];
+        int start=v[i][0];
+        int end=v[i][1];
+        int a=1e9,b=1e9;
+        if(start<=r){
+            a=1+fn(i+1,l,max(end,r),v,time);
+            b=fn(i+1,l,r,v,time);
+        }
+
+        return dp[i][l][r] = min(a,b);
+    }
+public:
+    int videoStitching(vector<vector<int>>& clips, int time) {
+        int n=clips.size();
+        sort(clips.begin(),clips.end());
+        memset(dp,-1,sizeof(dp));
+        int ans=fn(0,0,0,clips,time);
+        if(ans>=1e9)return -1;
+        return ans;
+    }
+};

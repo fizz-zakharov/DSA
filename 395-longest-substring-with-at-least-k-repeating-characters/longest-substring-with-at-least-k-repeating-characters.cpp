1a67240fa3d73ca1db1c5686bca428883a83c641
Ayush Sheel <sheelayush15@gmail.com>
2026-08-12 19:41:32 +0530

    Time: 6 ms (47.71%) | Memory: 12.1 MB (30.94%) - LeetSync

---
+++ 395-longest-substring-with-at-least-k-repeating-characters/longest-substring-with-at-least-k-repeating-characters.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int longestSubstring(string s, int k) {
+        int n=s.size();
+        if(k==1)return n;
+        vector<vector<int>> pre(n,vector<int>(26,0));
+        unordered_map<int,int> m;
+        int ans=0;
+        for(int i=0;i<n;i++){
+            pre[i][s[i]-'a']++;
+            m[s[i]-'a']=i;
+            if(i>0){
+                bool ch=false;
+                for(int j=0;j<26;j++){
+                    pre[i][j]+=pre[i-1][j];
+                    if(pre[i][j]>=k){
+                        ch=true;
+                    }
+                }
+                if(ch){
+                    int l=-1;
+                    bool changed=true;
+                    while(changed){
+                        changed=false;
+                        for(int j=0;j<26;j++){
+                            int cnt=pre[i][j]-(l==-1?0:pre[l][j]);
+                            if(cnt>0 && cnt<k){
+                                l=m[j];      // exclude this char entirely
+                                changed=true;
+                            }
+                        }
+                    }
+                    ans=max(ans,i-l);       // l==-1 naturally gives i+1
+                }
+            }
+        }
+        return ans;
+    }
+};

369d4cb8e1d710c088fccdfd7031502c7b13fd11
Ayush Sheel <sheelayush15@gmail.com>
2026-08-20 12:45:03 +0530

    Time: 42 ms (86.02%) | Memory: 29.1 MB (46.56%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -1,43 +1,33 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        int n = nums.size();
+        int n=nums.size();
+        sort(nums.begin(),nums.end());
         vector<vector<int>> ans;
-        map<int,int> m;
-        set<vector<int>> s;
-
-        sort(nums.begin(), nums.end());
-
-        for (int i = 0; i < n; i++)
-            m[nums[i]]++;
-
-        for (int i = 0; i < n; i++) {
-            if (i > 0 && nums[i] == nums[i - 1]) continue;
-            if (nums[i] > 0) break;
-
-            m[nums[i]]--;
-
-            for (int j = i + 1; j < n; j++) {
-                if (j > i + 1 && nums[j] == nums[j - 1]) continue;
-
-                m[nums[j]]--;
-
-                int need = -nums[i] - nums[j];
-                if (m[need] > 0) {
-                    vector<int> temp = {nums[i], nums[j], need};
-                    sort(temp.begin(), temp.end());
-                    s.insert(temp);
+        int temp=1e5+1;
+        for(int i=0;i<n;i++){
+            if(nums[i]>0)break;
+            if(i>0 && nums[i]==nums[i-1])continue;
+            int j=i+1,k=n-1;
+            while(j<n && k>=0 && j<k){
+                int sum=nums[i]+nums[j]+nums[k];
+                if(sum==0){
+                    ans.push_back({nums[i],nums[j],nums[k]});
+                    int temp1=nums[j];
+                    int temp2=nums[k];
+                    while(j<n && nums[j]==temp1){
+                        j++;
+                    }
+                    while(k>=0 && nums[k]==temp2){
+                        k--;
+                    }
                 }
-
-                m[nums[j]]++;
+                else if(sum<0){
+                    j++;
+                }
+                else k--;
             }
-
-            m[nums[i]]++;
         }
-
-        for (auto it : s)
-            ans.push_back(it);
-
         return ans;
     }
 };
